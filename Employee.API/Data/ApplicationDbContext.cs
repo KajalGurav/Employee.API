434fd46aa3e764e3b02d6b86aa58caac044904dc
mohinikadam22 <mohiniunmesh22@gmail.com>
2024-07-18 14:56:45 +0530

    Add project files.

---
+++ Employee.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Employee.API.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Employee.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        // commet
+       //anurag
+
+        //23456
+
+        //Rocky
+    }
+}
---
+++ Employee.API/Program.cs
@@ -0,0 +1,52 @@
+using Employee.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAllOrigins");
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication(); 
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
